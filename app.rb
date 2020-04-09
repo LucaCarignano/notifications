7e9d60df66dc2077c8a4389433c71220455b559c
LucaCarignano <lucamcarignano@gmail.com>
2020-04-09 20:29:53 -0300

    log-in

--- app.rb
+++ app.rb
@@ -1,20 +1,34 @@
 class App < Sinatra::Base
-    	get "/" do
-        	erb :log 
-    	end
-    	post "/" do
-    		if params[:user]=="Fede" && params[:pass] == "LUCA"
-				    "funciona"
-			else erb :log 			
-    		end
-    	end
-    	get "/users" do
-        	"hello users"
+	get "/" do
+    	erb :log 
 	end
-	get "/admin" do
-		"hello admins"
+
+	#Comprobar que el usuario y la contraseña sean del mismo user y se encuentre en
+	#la base de datos, si no, informar que se han ingresado datos inválidos
+	post "/" do
+		if params[:user]=="Fede" && params[:pass] == "LUCA" 
+			    "funciona"
+		else erb :login 			
+		end
 	end
-	get "/documents" do
-		"no documents yet"
+
+	#Comprobar que el usuario y la contraseña sean del mismo user y se encuentre en la base de datos
+	post "/login" do
+		if params[:users]=="Fede" && params[:passs] == "LUCA" 
+			    "funciona"
+		else erb :login
+		end
+	end
+
+	get "/rp" do
+		erb :rp
+	end
+	
+	#Comprobar que el mail se encuentre en la base de datos
+	post "/rp" do
+		if params[:email]=="Fede@gmail" 
+			    "se a enviado un correo a tu mail con la nueva contraseña provisoria"
+		else "email inválido"
+		end
 	end
 end

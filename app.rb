0c8c29caaeb9054919f1e2af405f6c88a4788b97
FedericoGutierrez101 <fede.a.gutierre1@gmail.com>
2020-04-09 18:43:03 -0300

    add log.html

--- app.rb
+++ app.rb
@@ -1,6 +1,12 @@
 class App < Sinatra::Base
     	get "/" do
-        	"hello cruel world!!!"
+        	erb :log 
+    	end
+    	post "/" do
+    		if params[:user]=="Fede" && params[:pass] == "LUCA"
+				    "funciona"
+			else erb :log 			
+    		end
     	end
     	get "/users" do
         	"hello users"

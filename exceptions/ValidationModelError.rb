15a2de8f5a12f7d820008dd38a6b30093f47031f
FedericoGutierrez101 <62314119+FedericoGutierrez101@users.noreply.github.com>
2020-11-09 21:53:50 -0300

    Revert "175653513 create exceptions"

--- exceptions/ValidationModelError.rb
+++
@@ -1,8 +0,0 @@
-class ValidationModelError < StandardError
-	attr_reader :errors
-
-	def initialize(msg = "Los datos ingresados no son correctos", erros)
-		super(msg)
-		@errors = errors
-	end
-end
--- models/user.rb
+++ models/user.rb
@@ -5,15 +5,10 @@ class User < Sequel::Model
   plugin :validation_helpers
   def validate
     super
-    validates_presence :name, message: "Debe indicarnos su numbre"
-    validates_presence :surname, message: "Debe indicarnos su apellido"
-    validates_presence :email, message: "Debe indicarnos su email"
-    validates_presence :password, message: "Debe ingresar una contraseña"
-    validates_presence :username, message: "Debe ingresar un nombre de usario"
-    validates_unique :email, message: "ya existe un usuario registrado con ese email"
-    validates_unique :username, message: "ya existe un usuario registrado con ese nombre de usuario"
-    validates_format (/ \A.*@.*\..*\z/), :email, message: 'invalid email'
-    validates_min_length 6, :password, message: 'password must have more than 5 caracters'
+    validates_presence %i[name surname email password username]
+    validates_unique %i[email username]
+    validates_format(/\A.*@.*\..*\z/, :email, message: 'invalid email')
+    validates_min_length(6, :password, message: 'password must have more than 5 caracters')
   end
   many_to_many :tags
   many_to_many :documents

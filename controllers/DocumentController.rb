80d9ca5c0fc5d44d1134040dfcb38d6c0491b893
FedericoGutierrez101 <fede.a.gutierre1@gmail.com>
2020-11-28 19:45:34 -0300

    [#175924014] refactor of /docs

--- controllers/DocumentController.rb
+++ controllers/DocumentController.rb
@@ -18,25 +18,6 @@ class DocumentController < Sinatra::Base
     set :sockets, []
   end
 
-#  get '/' do
-#    if !request.websocket?
-#      erb :log, layout: :layout_sig
-#    else
-#      request.websocket do |ws|
-#        user = session[:user_id]
-#        logger.info(user)
-#        @connection = { user: user, socket: ws }
-#        ws.onopen do
-#          settings.sockets << @connection
-#        end
-#        ws.onclose do
-#          warn('websocket closed')
-#          settings.sockets.delete(ws)
-#        end
-#      end
-#    end
-#  end
-
   get '/adddoc' do
     @current_user = User.find(id: session[:user_id])
     @admin = @current_user.admin
@@ -47,6 +28,18 @@ class DocumentController < Sinatra::Base
     end
     view_noti
     erb :add_doc, layout: :layout_main
+  end
+
+  get '/docs' do
+    @current_user = User.find(id: session[:user_id])
+    @admin = @current_user.admin
+    set_pages
+    cant_pages(Document.where(delete: 'f').count)
+    @documents = Document.order(:date).reverse.where(delete: 'f').limit(@docsperpage, (@page - 1) * @docsperpage)
+    @categories = Tag.all
+    @users = User.all
+    view_noti
+    erb :docs, layout: :layout_main
   end
 
   post '/adddoc' do
@@ -95,6 +88,66 @@ class DocumentController < Sinatra::Base
     end
   end
 
+  post '/docs' do
+    set_pages
+    redirect '/docs' if not_filter?
+    users_filter = params[:users]
+    tags_filter = params[:tags]
+    date_filter = params[:datedoc]
+    name_filter = params[:docname]
+    bot_filter = params[:filter]
+    del_doc = []
+    documents = Document.all
+      documents.each do |doc|
+        location = 'del' + doc.location
+        if params[location]
+          del_doc = doc 
+        end
+      end
+    @documents = DocumentService.filterDoc users_filter, tags_filter, date_filter, name_filter, bot_filter, del_doc
+    @categories = Tag.all
+    @users = User.all
+    view_noti
+    set_pages
+    cant_pages(@documents.length)
+    @documents = @documents[((@page - 1) * @docsperpage)..(@page * @docsperpage) - 1]
+    @current_user = User.find(id: session[:user_id])
+    view_noti
+    @admin = @current_user.admin
+    return erb :docs, layout: :layout_main
+  end
+
+  def not_filter?
+    params[:datedoc] == '' && params[:tags] == '' && params[:users] == '' && params[:filter] && params[:docname]
+  end
+
+  def set_pages
+    @page = if params[:page]
+              params[:page].to_i
+            else 1
+            end
+  end
+
+  def cant_pages(cantdocs)
+    @docsperpage = 10
+    @pagelimit = if (cantdocs % @docsperpage).zero?
+                   cantdocs / @docsperpage
+                 else
+                   cantdocs / @docsperpage + 1
+                 end
+  end
+
+  def doc_tags(docs_tags)
+    tags = []
+    docs_tags.each { |tag| tags << tag.name }
+    tags * ', '
+  end
+
+  def doc_users(docs_users)
+    users = []
+    docs_users.each { |user| users << user.username }
+    users * ', '
+  end
 
   def view_noti
     if @current_user
